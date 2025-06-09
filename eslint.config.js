de27069ad88d26fef22ea96547b181fb6b0c3dbb
muhammadubaidillah <muhammadubaidillah6@gmail.com>
2025-06-09 18:23:52 +0700

    Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
